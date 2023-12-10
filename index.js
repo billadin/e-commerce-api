6521baa208ec3f737d39a89bb4d0e36ce581629f
billadin <mir.billadin4@gmail.com>
2023-12-10 21:16:21 +0600

    database connection setup

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+// DB connection
+const connectDB = require('./db/connect');
+
+
+//Middleware
+app.use(express.json());
+app.use(cors())
+
+
 
 const port = process.env.port || 3000;
 
-app.listen(port, console.log(`Server is listening to ${port}...`))
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGO_URI);
+      app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  start();

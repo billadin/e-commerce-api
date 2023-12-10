5a4ec7fb764e4879304b8772b8ffde166dc264cc
billadin <mir.billadin4@gmail.com>
2023-12-10 22:44:05 +0600

    seller model setup

---
+++ models/Seller.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const SellerSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide name'],
+    maxlength: 50,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: [true, 'Please provide a role'],
+    minlength: 5,
+  },
+});
+
+
+module.exports = mongoose.model('Seller', SellerSchema);

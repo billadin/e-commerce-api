0f54c021b8f10453b650aa137329fbf02d3690c6
billadin <mir.billadin4@gmail.com>
2023-12-11 14:25:50 +0600

    updated product controller to seller

--- routes/seller.js
+++ routes/seller.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const addProduct = require('../controllers/product');
+const addProduct = require('../controllers/seller');
 const router = express.Router();
 
 router.route('/create-catalog').post(addProduct)

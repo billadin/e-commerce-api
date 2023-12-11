3d020270dc200a54dd84db417e3eab86269a795c
billadin <mir.billadin4@gmail.com>
2023-12-11 12:20:11 +0600

    validation error handler added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const BadRequestError = require("../errors/bad-request");
 const Buyer = require("../models/Buyer");
 const Seller = require("../models/Seller");
 const { StatusCodes } = require("http-status-codes");
@@ -5,16 +6,34 @@ const { StatusCodes } = require("http-status-codes");
 const register = async (req, res) => {
   const type = req.body.type;
 
-  if (type === "buyer") {
-    const user = await Buyer.create({ ...req.body });
-    res.status(StatusCodes.CREATED).json(user);
-  }
+  const user = await Buyer.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json(user);
 
-  if (type === "seller") {
-    const user = await Seller.create({ ...req.body });
-    res.status(StatusCodes.CREATED).json(user);
+  // if (type === "buyer") {
+  //   const user = await Buyer.create({ ...req.body });
+  //   res.status(StatusCodes.CREATED).json(user);
+  // }
+
+  // if (type === "seller") {
+  //   const user = await Seller.create({ ...req.body });
+  //   res.status(StatusCodes.CREATED).json(user);
+  // }
+
+};
+
+
+
+const login = async (req, res) => {
+
+  const { email, type } = req.body;
+
+  if (!email || !type) {
+    throw new BadRequestError('Please provide all details')
   }
+  const buyerUser = await Buyer.findOne({ email });
+  console.log(buyerUser)
+
   
 };
 
-module.exports = { register };
+module.exports = { register, login };
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,17 @@
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = async (err, req, res, next) => {
-  console.log(Object.values(err.keyValue));
+  console.log(Object.values(err.errors));
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong",
   };
+
+  if (err.name === 'ValidationError') {
+    const msg = err.errors.type.message
+    customError.msg = msg
+    customError.statusCode = 400
+  }
 
   if (err.code && err.code === 11000) {
     customError.msg = `${Object.keys(err.keyValue)} is already registered`;
